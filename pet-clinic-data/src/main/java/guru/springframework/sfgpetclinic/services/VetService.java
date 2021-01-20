56118af3a9494adbd685b4462f388d95d2fabeb2
Stephen <questioillustro@gmail.com>
2021-01-20 12:31:39 -0500

    Service interfaces

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
