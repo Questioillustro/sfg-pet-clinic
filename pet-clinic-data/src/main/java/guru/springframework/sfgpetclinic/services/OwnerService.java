5f47d7a6d7ec123e7c7c051b5a949bf2c2b1c07b
Stephen <questioillustro@gmail.com>
2021-01-20 15:27:08 -0500

    CrudService interface

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -3,16 +3,8 @@ package guru.springframework.sfgpetclinic.services;
 import guru.springframework.sfgpetclinic.model.Owner;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-
 @Service
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastname);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findaAll();
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -3,14 +3,6 @@ package guru.springframework.sfgpetclinic.services;
 import guru.springframework.sfgpetclinic.model.Pet;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-
 @Service
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
